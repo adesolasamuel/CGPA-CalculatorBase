355c14b6f6a662eef505bd05bb869017d5840d84
Samuel Adesola <55460620+adesolasamuel@users.noreply.github.com>
2020-06-23 16:48:23 +0100

    Add files via upload
    Main class

---
+++ Mainclass.h
@@ -0,0 +1,34 @@
+/*
+ * Mainclass.h
+ *of CGPA CALCULATOR
+ *
+ *  Created on: Apr 11, 2020   // ENDS ON Apr 15, 2020
+ *      Author: ADESOLA SAMUEL
+ */
+
+#ifndef MAINCLASS_H_
+#define MAINCLASS_H_
+#include <iostream>
+using namespace std;
+
+// THIS CLASS DEALS WITH THE CURRENT RESULTS AND GRADES
+
+class current{
+public:
+	current();
+
+	void getCourse();
+
+};
+
+
+// THIS CLASS DEALS WITH THE PREVIOUS CGPA
+
+class previous{
+public:
+	previous();
+	void getCourse_previous();
+};
+
+
+#endif /* MAINCLASS_H_ */
